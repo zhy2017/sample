b22bf2ded11dcec9b3d485b1e82bbd2130056f44
zhy <752188627@qq.com>
2018-05-17 06:12:09 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
 Route::get('/signup', 'UserController@create')->name('signup');
-Route::post('/test', 'StaticPagesController@test')->name('test');
 Route::resource('users', 'UserController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
